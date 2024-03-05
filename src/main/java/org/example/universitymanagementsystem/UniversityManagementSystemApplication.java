5ee4cc74bd204b8f7428f394d6a596a32d46524c
muzeffereli <muzeffereli@gmail.com>
2024-03-05 18:29:12 +0400

    security #2

--- src/main/java/org/example/universitymanagementsystem/UniversityManagementSystemApplication.java
+++ src/main/java/org/example/universitymanagementsystem/UniversityManagementSystemApplication.java
@@ -2,10 +2,10 @@ package org.example.universitymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true, jsr250Enabled = true)
+@EnableMethodSecurity
 public class UniversityManagementSystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/universitymanagementsystem/controller/InstructorController.java
+++ src/main/java/org/example/universitymanagementsystem/controller/InstructorController.java
@@ -7,6 +7,7 @@ import org.example.universitymanagementsystem.service.InstructorService;
 import org.example.universitymanagementsystem.shared.PageResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,6 +22,7 @@ public class InstructorController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('INSTRUCTOR', 'DEPARTMENT')")
     public ResponseEntity<InstructorDetailsDTO> findById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(instructorService.findById(id));
     }
